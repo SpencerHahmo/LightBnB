7d5d2f9de6afe6d84aa35598ce507d2e2fce957a
Spencer Hahmo <schahmo@gmail.com>
2023-03-09 16:47:51 -0700

    Refactor getAllReservations function to use the lightbnb database to return reservations associated with a specific user

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -83,9 +83,27 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+const getAllReservations = (guest_id, limit = 1) => {
+  const userValues = [guest_id, limit];
+  return pool
+    .query(`
+    SELECT reservations.*, properties.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+    `, userValues)
+    .then((result) => {
+      // console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
