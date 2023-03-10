c8f2a85621623e7d2e1b19ebd3fd8c82e3c6ca38
Spencer Hahmo <schahmo@gmail.com>
2023-03-09 19:58:52 -0700

    Refactor the getAllProperties function to accept arguments that will enable filtering

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -115,12 +115,51 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
+  const queryParams = [];
+  
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+  
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if(options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    queryString += `${queryString.includes("WHERE") ? 'AND owner_id = ' : 'WHERE owner_id = '} $${queryParams.length} `
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${Number(options.minimum_price_per_night)}`);
+    queryString += `${queryString.includes("WHERE") ? 'AND cost_per_night > ' : 'WHERE cost_per_night > '}$${queryParams.length} `
+  }
+  
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${Number(options.maximum_price_per_night)}`);
+    queryString += `${queryString.includes("WHERE") ? 'AND cost_per_night < ' : 'WHERE cost_per_night < '}$${queryParams.length} `
+  }
+  
+  if (options.minimum_rating) {
+    queryParams.push(`${Number(options.minimum_rating)}`);
+    queryString += `${queryString.includes("WHERE") ? 'AND rating >= ' : 'WHERE rating >= '}$${queryParams.length}`
+  }
+  
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  
+  console.log(queryString, queryParams);
+  
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      // console.log(result.rows);
-      return result.rows;
-    })
+    .query(queryString, queryParams)
+    .then((res) => res.rows)
     .catch((err) => {
       console.log(err.message);
     });
